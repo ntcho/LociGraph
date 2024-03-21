b7df897d64c9d0f6ec1e47402e98a369d0c82378
Nathan Cho <ntcho13@gmail.com>
2024-03-20 23:13:15 -0400

    feat: add mock ui

--- src/client/sidepanel.tsx
+++ src/client/sidepanel.tsx
@@ -1,27 +1,70 @@
 import { useState } from "react"
 
+import { Button } from "~components/ui/button"
+import { Input } from "~components/ui/input"
+import { Label } from "~components/ui/label"
+
+import "./style.css"
+
 function IndexSidePanel() {
-  const [data, setData] = useState("")
+  const [target, setTarget] = useState("")
+  const [relation, setRelation] = useState("")
+
+  const [response, setResponse] = useState(null)
+
+  const processPage = async () => {
+    // const response = await fetch("http://localhost:8080/process", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({ test: "test" })
+    // })
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    const data = await response.json()
+    setResponse(data)
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="flex flex-col items-stretch p-8 gap-4">
+      <h1 className="text-2xl font-bold">LociGraph</h1>
+
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="target">Target</Label>
+        <Input
+          type="text"
+          id="target"
+          placeholder="John Doe"
+          onChange={(e) => setTarget(e.target.value)}
+          value={target}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="relation">Relation</Label>
+        <Input
+          type="text"
+          id="relation"
+          placeholder="studied at"
+          onChange={(e) => setRelation(e.target.value)}
+          value={relation}
+        />
+      </div>
+
+      <Button
+        onClick={() => {
+          processPage()
+        }}>
+        Locate
+      </Button>
+
+      {response && (
+        <div>
+          <h3>Response</h3>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </div>
+      )}
     </div>
   )
 }
