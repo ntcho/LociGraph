81c23b92eddfb7d95c2a86e16b125e7572e310de
Nathan Cho <ntcho13@gmail.com>
2024-03-28 16:01:06 -0400

    feat: add support for multiple extraction models

--- src/server/extract.py
+++ src/server/extract.py
@@ -1,125 +1,87 @@
-from transformers import pipeline
-from dataclasses import dataclass
+import logging
 
-from dtos import Relation
+from utils.logging import FORMAT
 
-triplet_extractor = pipeline(
-    "translation_xx_to_yy",
-    model="Babelscape/mrebel-large",
-    tokenizer="Babelscape/mrebel-large",
-)
-# We need to use the tokenizer manually since we need special tokens.
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-@dataclass
-class Triplet:
-    head: str
-    head_type: str
-    type: str
-    tail: str
-    tail_type: str
+import requests
+from lxml.html import HtmlElement
+
+from dtos import ExtractionTarget, Relation
 
 
-# Function to parse the generated text and extract the triplets
-def extract_triplets_typed(text) -> list[Triplet]:
-    triplets = []
-    relation = ""
-    text = text.strip()
-    current = "x"
-    subject, relation, object_, object_type, subject_type = "", "", "", "", ""
+def extract_mrebel(
+    elements: list[HtmlElement], target: ExtractionTarget
+) -> list[Relation] | None:
+    """Extract relation triplets with mREBEL model.
 
-    for token in (
-        text.replace("<s>", "")
-        .replace("<pad>", "")
-        .replace("</s>", "")
-        .replace("tp_XX", "")
-        .replace("__en__", "")
-        .split()
-    ):
-        if token == "<triplet>" or token == "<relation>":
-            current = "t"
-            if relation != "":
-                triplets.append(
-                    Triplet(
-                        head=subject.strip(),
-                        head_type=subject_type,
-                        type=relation.strip(),
-                        tail=object_.strip(),
-                        tail_type=object_type,
-                    )
-                )
-                relation = ""
-            subject = ""
-        elif token.startswith("<") and token.endswith(">"):
-            if current == "t" or current == "o":
-                current = "s"
-                if relation != "":
-                    triplets.append(
-                        Triplet(
-                            head=subject.strip(),
-                            head_type=subject_type,
-                            type=relation.strip(),
-                            tail=object_.strip(),
-                            tail_type=object_type,
-                        )
-                    )
-                object_ = ""
-                subject_type = token[1:-1]
-            else:
-                current = "o"
-                object_type = token[1:-1]
-                relation = ""
-        else:
-            if current == "t":
-                subject += " " + token
-            elif current == "s":
-                object_ += " " + token
-            elif current == "o":
-                relation += " " + token
+    Args:
+        elements (list[HtmlElements]): List of HTML elements to extract relations from.
+        target (ExtractionTarget): The target entity to extract relations for.
 
-    if (
-        subject != ""
-        and relation != ""
-        and object_ != ""
-        and object_type != ""
-        and subject_type != ""
-    ):
-        triplets.append(
-            Triplet(
-                head=subject.strip(),
-                head_type=subject_type,
-                type=relation.strip(),
-                tail=object_.strip(),
-                tail_type=object_type,
+    Returns:
+        list[Relation]: List of extracted relations triplets or None if the
+        extraction failed.
+    """
+    element_contents: list[str] = [e.text_content() for e in elements]
+
+    response = requests.post(
+        "http://localhost:8001/extract/",
+        # send all text content of the relevant elements
+        data=(
+            "\n".join(
+                # filter elements with target entity (e.g. paragraph with entity name)
+                [e for e in element_contents if target.entity.lower() in e.lower()]
             )
+        ),
+    )
+
+    if response.status_code == 201:
+        # unpack json response to list of Relation objects
+        return [Relation(**r) for r in response.json()]
+    else:
+        logging.error(
+            f"Failed to extract relations. app_extract returned code {response.status_code}"
         )
+        return None
 
-    return triplets
 
+def extract_llm(
+    elements: list[HtmlElement], target: ExtractionTarget
+) -> list[Relation] | None:
+    """Extract relation triplets with LLMs.
 
-def extract(text: str):
-    extracted_text = triplet_extractor.tokenizer.batch_decode(
-        [
-            triplet_extractor(
-                text,
-                decoder_start_token_id=250058,
-                src_lang="en_XX",
-                tgt_lang="<triplet>",
-                return_tensors=True,
-                return_text=False,
-            )[0]["translation_token_ids"]
-        ]
-    )  # change en_XX for the language of the source.
-    extracted_triplets = extract_triplets_typed(extracted_text[0])
+    Args:
+        elements (list[HtmlElements]): List of HTML elements to extract relations from.
+        target (ExtractionTarget): The target entity to extract relations for.
 
-    #! DEBUG
-    print(extracted_triplets)
+    Returns:
+        list[Relation]: List of extracted relations triplets or None if the
+        extraction failed.
+    """
+    element_contents: list[str] = [e.text_content() for e in elements]
 
-    return [
-        Relation(
-            entity=t.head,
-            attribute=t.type,
-            value=t.tail,
-        )  # convert Triplet into Relation
-        for t in extracted_triplets
-    ]
+    return None  # TODO: add prompt for LLM extraction
+
+    # response = requests.post(
+    #     "http://localhost:8002/extract/",
+    #     # send all text content of the relevant elements
+    #     data=(
+    #         "\n".join(
+    #             # filter elements with target entity (e.g. paragraph with entity name)
+    #             [e for e in element_contents if target.entity.lower() in e.lower()]
+    #         )
+    #     ),
+    # )
+
+    # if response.status_code == 200:
+    #     # unpack json response to list of Relation objects
+    #     return [Relation(**r) for r in response.json()]
+    # else:
+    #     logging.error(
+    #         f"Failed to extract relations. API returned code {response.status_code}"
+    #     )
+    #     return None
---
+++ src/server/extract_mrebel.py
@@ -0,0 +1,172 @@
+import logging
+from utils.logging import FORMAT
+
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # TODO: Change to INFO on production
+
+
+from pprint import pformat
+
+from transformers import pipeline
+from dataclasses import dataclass
+
+from dtos import Relation
+
+triplet_extractor = pipeline(
+    "translation_xx_to_yy",
+    model="Babelscape/mrebel-large",
+    tokenizer="Babelscape/mrebel-large",
+)
+logger.info("Initialized triplet extractor pipeline")
+
+
+@dataclass
+class Triplet:
+    """Triplet dataclass to store the extracted triplets. Adds compatibility
+    between mREBEL outputs and Relation dataclass."""
+
+    head: str
+    head_type: str
+    type: str
+    tail: str
+    tail_type: str
+
+
+def extract(text: str) -> list[Relation]:
+    """Extract triplets from the given text.
+
+    Args:
+        text (str): The text to extract triplets from.
+
+    Returns:
+        list[Relation]: The extracted relations from the text.
+
+    Raises:
+        Exception: If the triplet extractor or tokenizer is not initialized.
+    """
+    if triplet_extractor is None or triplet_extractor.tokenizer is None:
+        raise Exception("Triplet extractor not initialized")
+
+    logger.info(f"Extracting triplets from text: \n```\n{text}\n```")
+
+    # Translate text into string with triplet tokens
+    extracted_text = triplet_extractor.tokenizer.batch_decode(
+        [
+            triplet_extractor(
+                text,
+                decoder_start_token_id=250058,  # `tp_XX` token
+                src_lang="en_XX",  # change en_XX for the language of the source
+                tgt_lang="<triplet>",
+                return_tensors=True,
+                return_text=False,
+            )[0]["translation_token_ids"]
+        ]
+    )
+
+    logger.debug(
+        f"Extracted text with triplet tokens: \n```\n{pformat(extracted_text)}\n```"
+    )
+
+    # Extract triplets from the translated text
+    triplets = extract_triplets(extracted_text[0])
+
+    logger.info(f"Extracted {len(triplets)} triplets from text")
+    logger.debug(f"Extracted triplets: \n```\n{pformat(triplets)}\n```")
+
+    return [
+        Relation(
+            entity=t.head,
+            attribute=t.type,
+            value=t.tail,
+        )  # convert Triplet into Relation
+        for t in triplets
+    ]
+
+
+def extract_triplets(extracted_text: str) -> list[Triplet]:
+    """Extract triplets from string with triplet tokens.
+
+    Note:
+        TODO: add `extracted_text` format and example
+
+    Args:
+        text (str): The translated text with triplet tokens.
+
+    Returns:
+        list[Triplet]: The extracted triplets from the text."""
+
+    triplets = []
+    relation = ""
+    extracted_text = extracted_text.strip()
+    current = "x"
+    subject, relation, object_, object_type, subject_type = "", "", "", "", ""
+
+    for token in (
+        # Remove special tokens
+        extracted_text.replace("<s>", "")
+        .replace("<pad>", "")
+        .replace("</s>", "")
+        .replace("tp_XX", "")
+        .replace("__en__", "")
+        .split()
+    ):
+        if token == "<triplet>" or token == "<relation>":
+            current = "t"
+            if relation != "":
+                triplets.append(
+                    Triplet(
+                        head=subject.strip(),
+                        head_type=subject_type,
+                        type=relation.strip(),
+                        tail=object_.strip(),
+                        tail_type=object_type,
+                    )
+                )
+                relation = ""
+            subject = ""
+        elif token.startswith("<") and token.endswith(">"):
+            if current == "t" or current == "o":
+                current = "s"
+                if relation != "":
+                    triplets.append(
+                        Triplet(
+                            head=subject.strip(),
+                            head_type=subject_type,
+                            type=relation.strip(),
+                            tail=object_.strip(),
+                            tail_type=object_type,
+                        )
+                    )
+                object_ = ""
+                subject_type = token[1:-1]
+            else:
+                current = "o"
+                object_type = token[1:-1]
+                relation = ""
+        else:
+            if current == "t":
+                subject += " " + token
+            elif current == "s":
+                object_ += " " + token
+            elif current == "o":
+                relation += " " + token
+
+    if (
+        subject != ""
+        and relation != ""
+        and object_ != ""
+        and object_type != ""
+        and subject_type != ""
+    ):
+        triplets.append(
+            Triplet(
+                head=subject.strip(),
+                head_type=subject_type,
+                type=relation.strip(),
+                tail=object_.strip(),
+                tail_type=object_type,
+            )
+        )
+
+    return triplets
