7e4c670359878f57126bc49d3b05d1cae6dceff5
Nathan Cho <ntcho13@gmail.com>
2024-03-28 16:08:24 -0400

    chore: refactor project structure

--- src/client/sidepanel/index.tsx
+++ src/client/sidepanel/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "~components/ui/button"
 import { Input } from "~components/ui/input"
 import { Label } from "~components/ui/label"
 
-import "./style.css"
+import "~style.css"
 
 function IndexSidePanel() {
   const [target, setTarget] = useState("")
