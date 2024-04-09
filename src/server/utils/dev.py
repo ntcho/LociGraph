cded9d101a4e48fa5260dfb7a837429960ddaa40
Nathan Cho <ntcho13@gmail.com>
2024-04-09 03:41:24 -0400

    dev: add timestamp util function

--- src/server/utils/dev.py
+++ src/server/utils/dev.py
@@ -1,4 +1,6 @@
 from base64 import b64encode
+from datetime import datetime
+
 
 def read_file_to_base64(file_path: str) -> str:
 
@@ -7,3 +9,7 @@ def read_file_to_base64(file_path: str) -> str:
         base64_string = b64encode(file_content).decode("utf-8")
 
         return base64_string
+
+
+def get_timestamp() -> str:
+    return datetime.now().strftime("%Y%m%d-%H%M%S")
