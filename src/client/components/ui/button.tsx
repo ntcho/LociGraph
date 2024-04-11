fe56a47dfa2687f7bfd4b1ec2fead82adeb68144
Nathan Cho <ntcho13@gmail.com>
2024-04-11 04:23:05 -0400

    feat: replace checkbox with toggle

--- src/client/components/ui/button.tsx
+++ src/client/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 
 import { cn } from "~lib/utils"
 
@@ -9,27 +9,31 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-stone-900 text-stone-50 hover:bg-stone-900/90 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/90",
+        default:
+          "bg-stone-900 text-stone-50 hover:bg-stone-900/90 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/90",
         destructive:
           "bg-red-500 text-stone-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-stone-50 dark:hover:bg-red-900/90",
+        warning:
+          "bg-orange-500 text-stone-50 hover:bg-orange-500/90 dark:bg-orange-700 dark:text-stone-50 dark:hover:bg-orange-700/90",
         outline:
           "border border-stone-200 bg-white hover:bg-stone-100 hover:text-stone-900 dark:border-stone-800 dark:bg-stone-950 dark:hover:bg-stone-800 dark:hover:text-stone-50",
         secondary:
           "bg-stone-100 text-stone-900 hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80",
-        ghost: "hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-800 dark:hover:text-stone-50",
-        link: "text-stone-900 underline-offset-4 hover:underline dark:text-stone-50",
+        ghost:
+          "hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-800 dark:hover:text-stone-50",
+        link: "text-stone-900 underline-offset-4 hover:underline dark:text-stone-50"
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
+        icon: "h-10 w-10"
+      }
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
-    },
+      size: "default"
+    }
   }
 )
 
--- src/client/sidepanel/index.tsx
+++ src/client/sidepanel/index.tsx
@@ -1,4 +1,4 @@
-import { AlertCircle } from "lucide-react"
+import { AlertCircle, AlertTriangle, LoaderCircle } from "lucide-react"
 import { useState } from "react"
 
 import { sendToBackground } from "@plasmohq/messaging"
@@ -7,9 +7,9 @@ import { ThemeProvider } from "~components/theme-provider"
 import { ThemeToggle } from "~components/theme-toggle"
 import { Alert, AlertDescription, AlertTitle } from "~components/ui/alert"
 import { Button } from "~components/ui/button"
-import { Checkbox } from "~components/ui/checkbox"
 import { Input } from "~components/ui/input"
 import { Label } from "~components/ui/label"
+import { Switch } from "~components/ui/switch"
 
 import "~style.css"
 
@@ -51,84 +51,95 @@ function IndexSidePanel() {
       defaultTheme="system"
       enableSystem
       disableTransitionOnChange>
-    <div className="flex flex-col items-stretch p-8 gap-4">
+      <div className="flex flex-col items-stretch p-8 gap-4">
         <div className="flex justify-between items-center">
-      <h1 className="text-2xl font-bold">LociGraph</h1>
+          <h1 className="text-2xl font-bold">LociGraph</h1>
           <ThemeToggle />
         </div>
 
-      <div className="flex flex-col gap-2">
-        <Label htmlFor="entity">Entity</Label>
-        <Input
-          type="text"
-          id="entity"
-          placeholder="John Doe"
-          disabled={isLoading}
-          onChange={(e) => setEntity(e.target.value)}
-          value={entity}
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Label htmlFor="attribute">Attribute</Label>
-        <Input
-          type="text"
-          id="attribute"
-          placeholder="studied at"
-          disabled={isLoading}
-          onChange={(e) => setAttribute(e.target.value)}
-          value={attribute}
-        />
-      </div>
-
-      <Button
-        disabled={isLoading}
-        onClick={() => {
-          processPage()
-        }}>
-        Locate
-      </Button>
-
-      <div className="items-top flex space-x-2">
-        <Checkbox
-          id="continuous"
-          checked={continuous}
-          onCheckedChange={(checked) => {
-            setContinuous(Boolean(checked))
-          }}
-        />
-        <div className="grid gap-1.5 leading-none">
-          <label
-            htmlFor="continuous"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-            Enable autonomous mode ⚡
-          </label>
-          <p className="text-xs text-muted-foreground leading-snug">
-            Allow LociGraph to autonomously navigate the web to find the
-            information you need.
-          </p>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="entity">Entity</Label>
+          <Input
+            type="text"
+            id="entity"
+            placeholder="John Doe"
+            disabled={isLoading}
+            onChange={(e) => setEntity(e.target.value)}
+            value={entity}
+          />
         </div>
-      </div>
 
-      {response && response.error && (
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>
-            {response.error
-              ? response.error
-              : "Unknown error occurred. Please try again later."}
-          </AlertDescription>
-        </Alert>
-      )}
-
-      {response && (
-        <div>
-          <h3>Response</h3>
-          <pre>{JSON.stringify(response, null, 2)}</pre>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="attribute">Attribute</Label>
+          <Input
+            type="text"
+            id="attribute"
+            placeholder="studied at"
+            disabled={isLoading}
+            onChange={(e) => setAttribute(e.target.value)}
+            value={attribute}
+          />
         </div>
-      )}
-    </div>
+
+        <div className="items-center flex gap-4">
+          <Switch
+            id="continuous-mode"
+            disabled={isLoading}
+            checked={continuous}
+            onCheckedChange={(checked) => {
+              setContinuous(Boolean(checked))
+            }}
+          />
+          <Label htmlFor="continuous-mode">Autonomous mode ⚡</Label>
+        </div>
+
+        <Button
+          variant={isLoading ? "outline" : continuous ? "warning" : "default"}
+          disabled={isLoading}
+          onClick={() => {
+            processPage()
+          }}>
+          {isLoading ? (
+            <div className="flex items-center">
+              <LoaderCircle className="h-4 w-4 mr-2 animate-spin" />
+              Processing...
+            </div>
+          ) : (
+            "Locate"
+          )}
+        </Button>
+
+        {continuous && (
+          <Alert variant="warning">
+            <AlertTriangle className="h-4 w-4" />
+            <AlertTitle>Warning</AlertTitle>
+            <AlertDescription>
+              Autonomous mode is enabled. LociGraph will interact with the page
+              without your input.
+            </AlertDescription>
+          </Alert>
+        )}
+
+        {response && response.error && (
+          <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+              {response.error
+                ? response.error
+                : "Unknown error occurred. Please try again later."}
+            </AlertDescription>
+          </Alert>
+        )}
+
+        {response && (
+          <div>
+            <h3>Response</h3>
+            <pre>{JSON.stringify(response, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+    </ThemeProvider>
   )
 }
 
