175b9aeabeaa2df8fc6157abe8e89e7e1ceb0751
Nathan Cho <ntcho13@gmail.com>
2024-04-22 19:18:49 -0400

    feat: add sidepanel shortcut

--- src/client/background/index.ts
+++ src/client/background/index.ts
@@ -1,1 +1,21 @@
 import "@plasmohq/messaging/background"
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'locigraph',
+    title: 'Browse with LociGraph',
+    contexts: ['page']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'locigraph') {
+    // This will open the panel in all the pages on the current window.
+    chrome.sidePanel.open({ windowId: tab.windowId });
+  }
+});
